d8d022c31a236f66d1e5235b51d74b0914777307
root <root@localhost>
2024-09-30 01:26:06 +0800

    auto publish 1727630766507

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Add all your JS here
+document.getElementById('magicButton').addEventListener('click', function() {
+    alert('Hoy bkla');
+    alert('anong cnasabe mo tongkol sakin. ha');
+    alert('bakla ayosin mo lang');
+    alert(`hello ma'am,, menu po natin today,,,,
+binaguongan baboy na may talong.
+sinigang na bangus dagupan.
+.adobong manok.
+lechon paksiw
+at dinuguan.
+may munggo poh may talbos ng ampalaya,,,,,`);
+});
